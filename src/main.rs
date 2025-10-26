9157ab3103318ed62e25dc1ad495af9f7a622e80
Kiwamu Okabe <kiwamu@gmail.com>
2025-10-27 07:36:29 +0900

    Bit

--- src/main.rs
+++ src/main.rs
@@ -75,7 +75,7 @@ fn show(tab: Arc<headless_chrome::browser::Tab>) {
     println!(" MMF: {}%", mmf_f / total_f * 100.0);
     println!("現金: {}%", money_f / total_f * 100.0);
 
-    println!("\n## 米国国債満期");
+    println!("\n## 米国債満期");
     v.sort();
     for i in &v {
         println!("{}年 {}円", i.0, i.1);
